d496cc060017a7a90be98b779d7844df00707d3b
Xian Jin <xianjinseow92@gmail.com>
2021-09-01 15:22:42 +0800

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { format, differenceInDays, compareAsc } from "date-fns";
+
+// Constants
+import { DATE_FORMAT } from 'constants/constants';
+
+import './App.scss';
+
 
 function App() {
+  const openDeadlineReminder = () => {
+    const deadline = new Date(2021, 8, 19); // month is 0-indexed
+    const today = new Date();
+
+    if (compareAsc(deadline, today) !== 1) {
+      alert("You passed your deadline boi!");
+    } else {
+      const daysLeft = differenceInDays(deadline, today);
+      alert(`You have your deadline is ${format(deadline, DATE_FORMAT)} and you have ${daysLeft} days left.`);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <header>
+          <h1>App initialized with Typescript and Bootstrap 5.1.0</h1>
+          <div className="w-100 d-flex">
+            <button className="btn btn-lg btn-dark ms-auto" onClick={openDeadlineReminder}>Click Me!</button>
+          </div>
+        </header>
+      </div>
     </div>
   );
 }
