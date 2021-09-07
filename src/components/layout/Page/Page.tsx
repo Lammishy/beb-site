3946e3163c458feced85afbc447f094cca0bb1dc
Xian Jin <xianjinseow92@gmail.com>
2021-09-07 20:08:58 +0800

    edit page component

--- src/components/layout/Page/Page.tsx
+++ src/components/layout/Page/Page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
 
 /**
  * Serves to center all elements in page
@@ -7,9 +7,9 @@ import Box from "@material-ui/core/Box";
 
 const Page = ({ children }: { children: any} ) => {
     return (
-        <Box display="flex" flexDirection="row" justifyContent="center" alignItems="center" width="100%">
+        <Container>
             {children}
-        </Box>
+        </Container>
     )
 }
 
