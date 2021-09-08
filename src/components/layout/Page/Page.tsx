dc70b9ad3b788515c708e42f47582113df0bb2a0
Xian Jin <xianjinseow92@gmail.com>
2021-09-08 22:47:02 +0800

    update common page component styles

--- src/components/layout/Page/Page.tsx
+++ src/components/layout/Page/Page.tsx
@@ -9,6 +9,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles({
     root: {
         display: "flex",
+        flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         minWidth: "100vw",
