52ea7cf813e93fd71ad4999292d56ea9f41a30e2
Xian Jin <xianjinseow92@gmail.com>
2021-09-16 13:57:45 +0800

    update logic for setting user as logged in

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import routes from "routes/routes";
 // Navigation around site
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 // Selector
 import { getLoginStatusSelector } from 'selectors/selectors';
@@ -19,22 +19,35 @@ import AuthPage from "components/pages/AuthPage";
 // Layout
 import BottomNavbar from "components/BottmNavbar/BottomNavbar";
 
+// Login-related
+import { loginSuccess } from "actions/actions";
+
+
 function App() {
   /**
    * Handles Routing of pages 
    * Global styles
    */
-
+  const dispatch = useDispatch();
   let isLoggedIn: boolean;
   isLoggedIn = useSelector(getLoginStatusSelector).isLoggedIn;
 
-  // Lazy to redo login logic that's tied to redux store. so just doing a hacky check here if we have already logged in previously (workaround for isLoggedIn variable being destroyed when we refresh page / new link)
-  if (!isLoggedIn) { 
-    // If login was already successful previously, then loginStatus should be inside session storage.
-    // Therefore, this code only really kicks in when user refreshes page / goes to new link (navigates around site)
+  // Lazy to redo login logic that's tied to redux store. 
+  // so just doing a hacky check here if we have
+  //  already logged in previously 
+  // (workaround for isLoggedIn variable being destroyed when we refresh page / new link)
+  if (!isLoggedIn) {
+    // If login was already successful previously, 
+    // then loginStatus should be inside session storage.
+    // Therefore, this code only really kicks in 
+    // when user refreshes page / goes to new link (navigates around site)
     const hasLoggedInBefore = sessionStorage.getItem("loginStatus")
-    isLoggedIn = hasLoggedInBefore ? true : isLoggedIn;
-   }
+
+    if (hasLoggedInBefore) {
+      // remember to dispatch to store to update absolute truth value about login
+      dispatch(loginSuccess());
+    }
+  }
 
   console.log("XJ App rendered");
 
