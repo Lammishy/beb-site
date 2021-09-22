334edb86c7a38ff9d469806d8b1bebc7cf42e898
Xian Jin <xianjinseow92@gmail.com>
2021-09-22 15:00:42 +0800

    remove comments and spaces

--- src/components/pages/AccessDeniedPage.tsx
+++ src/components/pages/AccessDeniedPage.tsx
@@ -50,12 +50,8 @@ const AccessDeniedPage = (props: any) => {
 
     const goBackToHomePageHandler = (event: any) => {
         // sends user back to login page. if already logged in, will send to home page (through redirect in router)
-        console.log("XJ goback is run");
-        // console.log("XJ history: ", history);
         history.push(ROUTE_NAMES.login);
     };
-
-    
 
     return (
         <Page>
