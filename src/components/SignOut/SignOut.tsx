4a7ab3b9000d45ed43089b8435f2768cd133ca79
Xian Jin <xianjinseow92@gmail.com>
2021-09-29 16:43:03 +0800

    Make Signout button responsive at 600 px width and lower (remove text, only leave icon)

--- src/components/SignOut/SignOut.tsx
+++ src/components/SignOut/SignOut.tsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 
 // Actions
@@ -5,32 +6,69 @@ import { loginFail } from "actions/actions";
 
 // Styles
 import { makeStyles } from "@material-ui/core";
+import theme from "styles/create-theme";
 
 // Component UI
 import { Button } from "@material-ui/core";
-import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 
 const SignOut = (props: any) => {
-    // Styles
-    const classes = makeStyles(() => ({
-        root: {
-            position: "absolute",
-            top: "20px",
-            right: "20px"
-        }
-    }))();
+  // Styles
+  const classes = makeStyles((theme) => ({
+    root: {
+      position: "absolute",
+      top: "20px",
+      right: "20px",
+    },
+    icon: {
+      [theme.breakpoints.down("xs")]: {
+        marginRight: 0,
+      },
+    },
+    label: {
+      [theme.breakpoints.down("xs")]: {
+        display: "flex",
+        marginLeft: "5px" // to center the button. it's off because the svg itself isn't centered
+      },
+    },
+  }))();
 
-    const dispatch = useDispatch();
+  const smBreakpoint = theme.breakpoints.values.sm;
+  const [width, setWidth] = useState(window.innerWidth);
 
-    const onSignOut = () => {
-        dispatch(loginFail());
+  // Handling window resize and changing button to only symbol
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      console.log("this is run");
     };
 
-    return <Button variant="contained" color="primary" className={classes.root} onClick={onSignOut} startIcon={<ExitToAppIcon/>}>
-        Get tha fak out
+    window.addEventListener("resize", handleResize);
+
+    // remove event listener
+    // otherwise you're creating multiple eventlisteners
+    // which execute code on the same event, exponentially increasing the number of calls to your code
+    return () => window.removeEventListener("resize", handleResize);
+  }, [width]);
+
+  const dispatch = useDispatch();
+
+  const onSignOut = () => {
+    dispatch(loginFail());
+  };
+
+  return (
+    <Button
+      variant="contained"
+      color="primary"
+      className={classes.root}
+      classes={{ startIcon: classes.icon, label: classes.label }}
+      onClick={onSignOut}
+      startIcon={<ExitToAppIcon />}
+    >
+      {width <= smBreakpoint ? "" : "Get tha fak out"}
     </Button>
-
+  );
 };
-
 
 export default SignOut;
