89ab1db781622afccba67c73c7ae683fe0d76b3c
Xian Jin <xianjinseow92@gmail.com>
2021-09-30 17:57:35 +0800

    more dramatic effect

--- src/components/pages/ThirdPage.tsx
+++ src/components/pages/ThirdPage.tsx
@@ -11,25 +11,28 @@ import MotionWrapper from "animations/MotionWrapper";
 import { ANIMATION_DURATION } from "animations/constants/constants";
 
 const ThirdPage = (props: any) => {
-    const classes = makeStyles({
-        center: {
-            textAlign: "center"
-        }
-    })();
+  const classes = makeStyles({
+    center: {
+      textAlign: "center",
+    },
+  })();
 
-    return (
-        <Page>
-            <Typography variant="h1" className={classes.center}>
-                <b>THIS AUTUMN</b>
-                <br />
-                <br />
-                <MotionWrapper transition={{delay: `${ANIMATION_DURATION * 2.5}`}}>
-                    2nd October, 2021
-                    {/* <b>{dateToDisplay}</b> */}
-                </MotionWrapper>
-            </Typography>
-        </Page>
-    )
-}
+  return (
+    <Page>
+      <Typography variant="h1" className={classes.center}>
+        <b>THIS AUTUMN</b>
+        <br />
+        <br />
+        <MotionWrapper transition={{ delay: `${ANIMATION_DURATION * 2.5}` }}>
+          2<sup>nd</sup> October{" "}
+          <MotionWrapper transition={{ delay: `${ANIMATION_DURATION * 5.5}` }}>
+            <b>2021</b>
+          </MotionWrapper>
+          {/* <b>{dateToDisplay}</b> */}
+        </MotionWrapper>
+      </Typography>
+    </Page>
+  );
+};
 
 export default ThirdPage;
