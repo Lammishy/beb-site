3e2ce675fb3f84f4a02b466a09f1eec8b643f4bd
Xian Jin <xianjinseow92@gmail.com>
2021-10-04 14:49:41 +0800

    fix text exceeding page height for journey start page

--- src/components/pages/JourneyStartPage.tsx
+++ src/components/pages/JourneyStartPage.tsx
@@ -1,27 +1,38 @@
 import Page from "components/layout/Page/Page";
 import { Typography } from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 // Animation
 import MotionWrapper from "animations/MotionWrapper";
 import { ANIMATION_DURATION } from "animations/constants/constants";
 
 const JourneyStartPage = (props: any) => {
-    const classes = makeStyles({
-        center: {
-            textAlign: "center"
-        }
-    })();
+  const classes = makeStyles({
+    center: {
+      textAlign: "center",
+    },
+  })();
 
-    return <Page>
-        <Typography variant="h1" className={classes.center}>
-            <b>Top reasons</b> 
-            <MotionWrapper transition={{delay: ANIMATION_DURATION * 7}}>
-                for why the beb should stop stealing the beeb's food
-            </MotionWrapper>
-        </Typography>
+  const responsiveFont = makeStyles((theme) => ({
+    root: {
+      [theme.breakpoints.down("xs")]: {
+        fontSize: "3rem",
+      },
+    },
+  }))();
+
+  return (
+    <Page>
+      <Typography variant="h1" className={classes.center}>
+        <b>Top reasons</b>
+        <MotionWrapper transition={{ delay: ANIMATION_DURATION * 7 }}>
+          <Typography variant="h1" className={responsiveFont.root}>
+            for why the beb should stop stealing the beeb's food
+          </Typography>
+        </MotionWrapper>
+      </Typography>
     </Page>
-}
-
+  );
+};
 
 export default JourneyStartPage;
