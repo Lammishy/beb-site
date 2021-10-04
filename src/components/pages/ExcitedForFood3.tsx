0e3eba893c0a39542c65e2a88ecd0927db8a0461
Xian Jin <xianjinseow92@gmail.com>
2021-10-04 15:10:52 +0800

    fix text overflowing page for excited for food 3 page

--- src/components/pages/ExcitedForFood3.tsx
+++ src/components/pages/ExcitedForFood3.tsx
@@ -4,18 +4,18 @@
 import { Typography } from "@material-ui/core";
 
 // Styles
-import { makeStyles } from "@material-ui/styles";
+import { makeStyles } from "@material-ui/core/styles";
 
 // Layout
 import Page from "components/layout/Page/Page";
 
 const ThirdPage = (props: any) => {
-  const classes = makeStyles((_) => ({
+  const classes = makeStyles((theme) => ({
     root: {
       display: "flex",
       justifyContent: "space-around",
       marginBottom: "40px",
-      overflow: "hidden",
+      overflow: "hidden"
     },
     imageList: {
       width: 500,
@@ -23,7 +23,10 @@ const ThirdPage = (props: any) => {
     },
     center: {
       textAlign: "center",
-    },
+      [theme.breakpoints.down("xs")]: {
+        fontSize: "3rem"
+      }
+    }
   }))();
 
   return (
